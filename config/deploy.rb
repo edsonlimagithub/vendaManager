d26dd1a83c5d1ad5addf5014830477dd15b8afae
edson <edson@edson-acer.(none)>
2012-04-20 19:25:30 -0300

    teste de deploy 20/04

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ role :app, "50.116.36.53"                          # This may be the same as you
 role :db,  "50.116.36.53", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
-set :user, "root"
+set :user, "edson"
 set :use_sudo, false
 
 # if you want to clean up old releases on each deploy uncomment this:
@@ -22,10 +22,10 @@ set :use_sudo, false
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
