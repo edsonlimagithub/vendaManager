c771878b099dcde546c1bacedddcb22b99eebd42
edson <edson@edson-acer.(none)>
2012-04-25 19:49:25 -0300

    mais teste

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,8 +12,10 @@ role :app, "50.116.36.53"                          # This may be the same as you
 role :db,  "50.116.36.53", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
-set :user, "edson"
+set :user, "root"
 set :use_sudo, false
+set :deploy_via, :copy
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
